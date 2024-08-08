59eb846034c542884bf3b789aecc9c746fcbf2f1
AlphonsaJo <91889313+AlphonsaJo@users.noreply.github.com>
2024-08-08 23:44:06 +0530

    Update 15_Fractional_Knapsack.py

--- 15_Fractional_Knapsack.py
+++ 15_Fractional_Knapsack.py
@@ -1,1 +1,17 @@
 
+def fractional_knapsack(items, capacity):
+    # Sort items by value to weight ratio in descending order
+    items.sort(key=lambda item: item.ratio, reverse=True)
+
+    total_value = 0.0  # Total value in the knapsack
+    for item in items:
+        if capacity - item.weight >= 0:
+            # If the item can fit, add it to the knapsack
+            capacity -= item.weight
+            total_value += item.value
+        else:
+            # If the item can't fit, take the fractional part of it
+            total_value += item.ratio * capacity
+            break
+
+    return total_value
