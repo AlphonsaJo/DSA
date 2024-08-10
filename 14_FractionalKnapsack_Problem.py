4b0ee107e32bb03b1e9c267dd877bbe4d97e9eaa
AlphonsaJo <91889313+AlphonsaJo@users.noreply.github.com>
2024-08-10 23:52:06 +0530

    Update 14_FractionalKnapsack_Problem.py

--- 14_FractionalKnapsack_Problem.py
+++ 14_FractionalKnapsack_Problem.py
@@ -7,3 +7,29 @@ You are also given an integer truckSize, which is the maximum number of boxes th
 
 Return the maximum total number of units that can be put on the tru
 '''
+
+def fill_truck(DiffBoxes, truck_size):
+
+    DiffBoxes.sort(key=lambda x: x[1], reverse=True)
+    
+    total_units = 0
+    
+    for boxes, units in DiffBoxes:
+
+        boxes_to_fill_with = min(boxes, truck_size)
+
+        total_units += boxes_to_fill_with * units
+    
+        truck_size -= boxes_to_fill_with
+
+        if truck_size == 0:
+            break
+    
+    return total_units
+
+
+DiffBoxes = [[1, 5], [2, 3]]
+truck_size = 11
+
+max_fill = fill_truck(DiffBoxes, truck_size)
+print(max_fill)  
