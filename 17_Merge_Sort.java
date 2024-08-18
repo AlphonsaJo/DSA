ea4200ae05276719173e00e7e65c3d093f97871c
AlphonsaJo <91889313+AlphonsaJo@users.noreply.github.com>
2024-08-18 22:56:09 +0530

    Update 17_Merge_Sort.java

--- 17_Merge_Sort.java
+++ 17_Merge_Sort.java
@@ -1,3 +1,30 @@
+/*
+
+is more predictable in terms of performance and is stable,.
+It’s particularly useful when stability is a requirement or when working with linked lists.
+
+Algorithm Type: Comparison-based, divide-and-conquer.
+Approach:
+
+    Dividing: Merge Sort divides the array into two halves until each sub-array contains a single element.
+    Merging: It then merges these sub-arrays to produce sorted arrays.
+
+Time Complexity:
+
+    All Cases: O(n log n) (Merge Sort has consistent performance regardless of the input data)
+
+Space Complexity: O(n) due to the need for temporary arrays used during merging.
+Stability: Stable. Equal elements retain their relative order.
+Not In-Place: Merge Sort requires additional space proportional to the size of the array.
+
+Applications:
+Data Analysis: Applied in sorting large-scale data for statistical analysis and data processing tasks.
+Computational Geometry: Used in algorithms for tasks like finding intersections and ordering geometric objects.
+Multithreaded Sorting: Beneficial for parallel processing and multithreaded environments due to its divide-and-conquer 
+approach that can be efficiently parallelized.
+
+*/
+
 public class MergeSort {
 
     // Main function to sort an array using merge sort
