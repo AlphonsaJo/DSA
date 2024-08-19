7c6b9ff4d11203dbbe01a5a8eaf75e58ef92b802
AlphonsaJo <91889313+AlphonsaJo@users.noreply.github.com>
2024-08-19 22:54:32 +0530

    Create 19_Middle_Element_Of_An_Array.java

---
+++ 19_Middle_Element_Of_An_Array.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+// Creation of a singly-linked list node that consists of fields, data and next
+class Node {
+    int data;
+    Node next;
+
+    // Constructor to initialize a new node with data and next pointer pointing to null
+  // this keyword is a reference to the current object instance within a class.
+    Node(int new_data) {
+        this.data = new_data;
+        this.next = null;
+    }
+}
