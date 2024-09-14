08a229eab715209f90ae64239354aed4ebdd4aa2
AlphonsaJo <91889313+AlphonsaJo@users.noreply.github.com>
2024-09-14 13:03:05 +0530

    Update 27_Insertion_Sort.py

--- 27_Insertion_Sort.py
+++ 27_Insertion_Sort.py
@@ -1,13 +1,32 @@
 '''
-Compare the current element with the elements in the sorted section (elements before it i.e. LHS) and shift those elements to the right 
-until the correct position for the current element is found.
+Compare elements to the left and shift elements to the right to insert element. 
 
 Outer loop - iterates through each element starting from index 1.
 Inner loop - shifts elements of the sorted section to the right if they are larger than the current element.
 
 Time and Space Complexity:
 Best-case time complexity: O(n) - All elements are in their correct positions.
-Worst-case time complexity: O(n^2) - All element are unsorted.
+Worst-case time complexity: O(n^2) - All element are unsorted. ~ i.e. decent with small datasets, bad with large datasets
 Average-case time complexity: O(n^2) - Elements are in random order and the time complexity will be quadratic in nature as well.
 Space complexity: O(1) - In-place Sorting - does nor require additional space for sorting. 
-''
+'''
+def insertion_sort(arr):
+    n = len(arr)
+    
+    for i in range(1, n):
+        temp = arr[i]  
+        j = i - 1
+        
+        
+        while j >= 0 and arr[j] > temp:
+            arr[j + 1] = arr[j]
+            j -= 1
+        
+       
+        arr[j + 1] = temp
+    
+    return arr
+
+arr = [2, 3, 4, 1, 8]
+print(insertion_sort(arr))
+
