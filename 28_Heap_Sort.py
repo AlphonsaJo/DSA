b7bca5329ff4593a9fc7c631c8354629cb4d6e1a
AlphonsaJo <91889313+AlphonsaJo@users.noreply.github.com>
2024-09-17 00:09:42 +0530

    Update 28_Heap_Sort.py

--- 28_Heap_Sort.py
+++ 28_Heap_Sort.py
@@ -1,1 +1,10 @@
+'''
+Time Complexity:
 
+    Building the Heap: O(n), where n is the number of elements in the array.
+    Heapify Operations: O(log n) per extraction, and since there are n elements, the total complexity for sorting is O(n log n).
+
+Space Complexity: Heap sort has a space complexity of O(1), meaning it sorts the array in-place without requiring additional storage beyond the input array.
+
+Stability: Heap sort is not a stable sorting algorithm, meaning it does not necessarily preserve the relative order of equal elements.
+'''
