5fb16032c4ead5ce4319369ac4b3c8b96e8425b8
AlphonsaJo <91889313+AlphonsaJo@users.noreply.github.com>
2024-09-18 20:50:07 +0530

    Update 28_Heap_Sort.py

--- 28_Heap_Sort.py
+++ 28_Heap_Sort.py
@@ -28,4 +28,41 @@ This is more efficient than starting at the root.
 '''
 
 
+# Function to heapify a subtree rooted at index i
+def heapify(arr, n, i):
+    largest = i  
+    left = 2 * i + 1  # Left child index
+    right = 2 * i + 2  # Right child index
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    # If right child is larger than largest so far
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]  
+
+
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+
+    # Build a maxheap
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    # Extract elements one by one
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]  # Swap
+        heapify(arr, i, 0)
+
+
+arr = [12, 11, 13, 5, 6, 7]
+heap_sort(arr)
+print("Sorted array is:", arr)
+
+
 
