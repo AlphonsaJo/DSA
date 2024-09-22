689d57aac3dbeb922efe93079c732ca52f22d09c
AlphonsaJo <91889313+AlphonsaJo@users.noreply.github.com>
2024-09-22 19:31:20 +0530

    Update Bucket_Sort.py

--- Bucket_Sort.py
+++ Bucket_Sort.py
@@ -1,1 +1,18 @@
+def bucket_sort(arr):
+  
+    num_buckets = len(arr)
+    buckets = [[] for _ in range(num_buckets)]
+
+    for num in arr:
+        index = int(num * num_buckets)
+        buckets[index].append(num)
+
+    for bucket in buckets:
+        bucket.sort()
+
+    sorted_array = []
+    for bucket in buckets:
+        sorted_array.extend(bucket)
+
+    return sorted_array
 
