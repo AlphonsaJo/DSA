ea33ac180d5d306dd2c819f91e19bbab31209989
AlphonsaJo <91889313+AlphonsaJo@users.noreply.github.com>
2024-09-26 23:56:40 +0530

    Update 31_AVL.java

--- 31_AVL.java
+++ 31_AVL.java
@@ -8,3 +8,26 @@ class Node {
     }
 }
 
+class AVLTree {
+
+    int height(Node N) {
+        if (N == null)
+            return 0;
+        return N.height;
+    }
+
+  
+    Node rightRotate(Node y) {
+        Node x = y.left;
+        Node T2 = x.right;
+
+
+        x.right = y;
+        y.left = T2;
+
+
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+
+        return x;
+    }
